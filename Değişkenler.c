603f2aca4f87a4a0a356e2ef4444d006883e34ff
MZTURK <34200422+MZTURK@users.noreply.github.com>
2020-09-04 23:22:12 +0300

    Update and rename HelloWorld.c to Değişkenler.c

---
+++ Değişkenler.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int sayici=0;
+int sayi1=1;
+float sayi2=2;
+double sayi 3=3;
+uint8_t sayi4=4;
+uint16_t sayi5=5;
+uint32_t sayi6=6;
+
+int main(){
+	
+	
+	printf("sayici degişkeninin degeri : %d\n",sayici);
+	printf("sayi1 degişkeninin degeri : %d\n",sayi1);
+	printf("sayi2 degişkeninin degeri : %d\n",sayi2);
+	printf("sayi3 degişkeninin degeri : %d\n",sayi3);
+	printf("sayi4 degişkeninin degeri : %d\n",sayi4);
+	printf("sayi6 degişkeninin degeri : %d\n",sayi6);
+	
+	
+	return 0;
+}
